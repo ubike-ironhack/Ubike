0d2daf2340ed2500d111b6f846a07af14095301e
dannbleu <d_stm@hotmail.com>
2019-04-15 13:55:16 -0500

    primer commit pre

--- Ubike/models/User.js
+++ Ubike/models/User.js
@@ -1,14 +1,32 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
   username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+  name: {
+    type: String,
+    required: true,
+  }, 
+  lastname: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password:{
+    type: password,
+    required: true
+  },
+  image: {
+    type: String
+  }, 
+  timestamps: true}
+  );
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email",
+  hashField: "password"
 });
 
 const User = mongoose.model('User', userSchema);
