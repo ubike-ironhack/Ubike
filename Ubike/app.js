1247df2fdc00bb33f2e180cd8967c6e1c5877b78
luisf89 <luisf.martinez89@gmail.com>
2019-04-15 16:38:04 -0500

    ¨chale¨

--- Ubike/app.js
+++ Ubike/app.js
@@ -12,16 +12,16 @@ const path         = require('path');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
-    
+
 
 mongoose
-  .connect('mongodb://localhost/ubike', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+ .connect('mongodb://localhost/ubike', {useNewUrlParser: true})
+ .then(x => {
+   console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+ })
+ .catch(err => {
+   console.error('Error connecting to mongo', err)
+ });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -37,11 +37,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+ src:  path.join(__dirname, 'public'),
+ dest: path.join(__dirname, 'public'),
+ sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -50,15 +50,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
+ if (arguments.length < 2)
+     throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+ if (typeof value !== undefined ) {
+     return options.inverse(this);
+ } else {
+     return options.fn(this);
+ }
 });
-  
+
 
 // default value for title local
 app.locals.title = 'Ubike';
@@ -66,20 +66,20 @@ app.locals.title = 'Ubike';
 
 // Enable authentication using session + passport
 app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
+ secret: 'irongenerator',
+ resave: true,
+ saveUninitialized: true,
+ store: new MongoStore( { mongooseConnection: mongoose.connection })
 }))
 app.use(flash());
 require('./passport')(app);
-    
+
 
 const index = require('./routes/index');
 app.use('/', index);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
-      
+
 
 module.exports = app;
