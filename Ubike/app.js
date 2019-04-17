797954ef9076f9a8c3bae06ffea1e3436cf760a9
luisf89 <luisf.martinez89@gmail.com>
2019-04-16 20:24:29 -0500

    Ya se ven más trucutru la landing

--- Ubike/app.js
+++ Ubike/app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
 
 
 mongoose
- .connect('mongodb://localhost/ubike', {useNewUrlParser: true})
+ .connect(process.env.DB, {useNewUrlParser: true})
  .then(x => {
    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
  })
